e6d929d87781510b74720dcf9b7b3ddb2f3d38c9
Tinh <tinh.chau3003@hcmut.edu.vn>
2023-07-21 22:01:16 +0700

    [TinhChau] add Driver/GPIO_Driver

---
+++ Driver/main/app_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "GPIO_Driver.h"
+
+void app_main(void)
+{
+
+}
