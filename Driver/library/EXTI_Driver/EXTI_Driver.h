d86b0343615b2b54e65dd58c05ba02fedd0ca01d
Tinh <tinh.chau3003@hcmut.edu.vn>
2023-07-24 09:01:17 +0700

    [TinhChau] add Driver/EXTI Driver

--- Driver/library/EXTI_Driver/EXTI_Driver.h
+++ Driver/library/EXTI_Driver/EXTI_Driver.h
@@ -7,9 +7,12 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-xQueueHandle xQueue_ExternalInterrupt;
-
+void (*EXTI_RisingEdgeDectect_Handle) (gpio_num_t EXTI_Pin);
+void (*EXTI_FallingEdgeDectect_Handle) (gpio_num_t EXTI_Pin);
+void EXTI_InitFunction( void (*RisingEdgeDectect_Handle) (gpio_num_t EXTI_Pin), 
+                        void (*FallingEdgeDectect_Handle) (gpio_num_t EXTI_Pin) );
 static void IRAM_ATTR external_interrupt_handler(void *args);
+
 void external_interrupt_task(void *pvParameter);
 void external_interrupt_initialize(gpio_num_t gpio_pin, gpio_int_type_t intr_edge);
 
--- Driver/main/app_main.c
+++ Driver/main/app_main.c
@@ -7,9 +7,53 @@
 #include "GPIO_Driver.h"
 #include "EXTI_Driver.h"
 
-static void IRAM_ATTR external_interrupt_handler(void *args);
+// static void IRAM_ATTR external_interrupt_handler(void *args);
+
+uint16_t count_button1;
+uint16_t count_button2;
+uint16_t count_button3;
+
+void FallingEdge(gpio_num_t pinNumber)
+{
+    switch (pinNumber)
+    {
+    case GPIO_NUM_5:
+        printf("Button %d pressed -- %d times\n", pinNumber, count_button1++);
+        break;
+    
+    case GPIO_NUM_18:
+        printf("Button %d pressed -- %d times\n", pinNumber, count_button2++);
+        break;
+
+    case GPIO_NUM_19:
+        printf("Button %d pressed -- %d times\n", pinNumber, count_button3++);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void RisingEdge(gpio_num_t pinNumber)
+{
+    switch (pinNumber)
+    {
+    case GPIO_NUM_5:
+        printf("Button %d release\n", pinNumber);
+        break;
+    case GPIO_NUM_18:
+        break;
+    case GPIO_NUM_19:
+        break;
+    default:
+        break;
+    }
+}
 
 void app_main(void)
 {
-    external_interrupt_initialize(GPIO_NUM_5, GPIO_INTR_NEGEDGE);
+    EXTI_InitFunction(RisingEdge, FallingEdge);
+    external_interrupt_initialize(GPIO_NUM_5, GPIO_INTR_ANYEDGE);
+    external_interrupt_initialize(GPIO_NUM_18, GPIO_INTR_NEGEDGE);
+    external_interrupt_initialize(GPIO_NUM_19, GPIO_INTR_NEGEDGE);
 }
